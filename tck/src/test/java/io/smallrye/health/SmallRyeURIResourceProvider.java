273ae83b1205ddf671889e447fc890c5db83d73e
Sergey Beryozkin <sberyozkin@gmail.com>
2018-06-05 17:20:20 +0100

    Starting with the initial implementation

---
+++ tck/src/test/java/io/smallrye/health/SmallRyeURIResourceProvider.java
@@ -0,0 +1,37 @@
+package io.smallrye.health;
+
+import java.lang.annotation.Annotation;
+import java.net.Inet6Address;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Set;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.test.spi.enricher.resource.ResourceProvider;
+
+
+public class SmallRyeURIResourceProvider implements ResourceProvider {
+    @Override
+    public boolean canProvide(Class<?> type) {
+        return type.isAssignableFrom(URI.class);
+    }
+
+    @Override
+    public Object lookup(ArquillianResource resource, Annotation... qualifiers) {
+        return null;
+    }
+
+    URI toURI(InetSocketAddress addr) throws URISyntaxException {
+        if (addr.getAddress() instanceof Inet6Address) {
+            return new URI("http://[" + addr.getHostName() + "]:" + addr.getPort() + "/");
+        }
+        return new URI("http://" + addr.getHostName() + ":" + addr.getPort() + "/");
+    }
+}
