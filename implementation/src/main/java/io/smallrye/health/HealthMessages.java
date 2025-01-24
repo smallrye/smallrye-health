19da26960a281997daa63945df5077f77a7a0e74
xstefank <xstefank122@gmail.com>
2025-01-24 13:52:27 +0100

    Remove deprecated use of jboss-logging methods

--- implementation/src/main/java/io/smallrye/health/HealthMessages.java
+++ implementation/src/main/java/io/smallrye/health/HealthMessages.java
@@ -1,12 +1,14 @@
 package io.smallrye.health;
 
+import java.lang.invoke.MethodHandles;
+
 import org.jboss.logging.Messages;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageBundle;
 
 @MessageBundle(projectCode = "SRHCK", length = 5)
 interface HealthMessages {
-    HealthMessages msg = Messages.getBundle(HealthMessages.class);
+    HealthMessages msg = Messages.getBundle(MethodHandles.lookup(), HealthMessages.class);
 
     @Message(id = 0, value = "Health Check contains an invalid name. Can not be null or empty.")
     IllegalArgumentException invalidHealthCheckName();
